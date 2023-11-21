f5a02eb1fd1909e044c2232f1d6a8cd8ad68523b
Mohit Singh <msakra000@gmail.com>
2023-11-21 21:50:19 +0530

    some minor fixes!

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,7 +67,17 @@ document.addEventListener('visibilitychange',
             $("#icon").attr("href", "assets/images/favhand.png");
         }
     });
-
+// document.addEventListener('hover',
+//     function () {
+//         if (document.visibilityState === "visible") {
+//             document.title = "Portfolio | Mohit Singh";
+//             $("#icon").attr("href", "assets/images/icon.png");
+//         }
+//         else {
+//             document.title = "Come Back To Portfolio";
+//             $("#icon").attr("href", "assets/images/favhand.png");
+//         }
+//     });
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
@@ -172,33 +182,33 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
 // pre loader end
 
 // disable developer mode
-document.onkeydown = function (e) {
-    if (e.keyCode == 123) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-        return false;
-    }
-}
+// document.onkeydown = function (e) {
+//     if (e.keyCode == 123) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//         return false;
+//     }
+// }
 
 // Start of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6506d5a2b1aaa13b7a774f7a/1hahb516r';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/6506d5a2b1aaa13b7a774f7a/1hahb516r';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
 })();
 
 
