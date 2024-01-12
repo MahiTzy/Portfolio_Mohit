142afe0d43327eb94f3c362decb128bb870dbc51
Mohit Singh <msakra000@gmail.com>
2024-01-12 17:51:51 +0530

    small changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -158,17 +158,6 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
 });
 // <!-- tilt js effect ends -->
 
-
-// pre loader start
-// function loader() {
-//     document.querySelector('.loader-container').classList.add('fade-out');
-// }
-// function fadeOut() {
-//     setInterval(loader, 500);
-// }
-// window.onload = fadeOut;
-// pre loader end
-
 // disable developer mode
 document.onkeydown = function (e) {
     if (e.keyCode == 123) {
