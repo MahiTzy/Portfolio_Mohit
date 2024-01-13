373b80eb12a8e616ccc9d90632ecc69d674f249b
Mohit Singh <msakra000@gmail.com>
2024-01-13 17:03:48 +0530

    optimization

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Mohit Singh | Developer";
-            $("#icon").attr("href", "assets/images/icon.png");
+            document.title = "MOHIT SINGH | PORTFOLIO | WEB DEVELOPER";
+            $("#icon").attr("href", "assets/images/favicon.ico");
         }
         else {
             document.title = "Come Back To Portfolio";
