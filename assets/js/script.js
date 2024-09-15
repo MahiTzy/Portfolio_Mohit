14f2a3ce758414610146df3033892ca4e4a834bb
Mohit Singh <msakra000@gmail.com>
2024-09-15 19:20:16 +0530

    updated title

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "MOHIT SINGH | PORTFOLIO | WEB DEVELOPER";
+            document.title = "Mohit Singh | Full-Stack Developer";
             $("#icon").attr("href", "assets/images/favicon.ico");
         }
         else {
